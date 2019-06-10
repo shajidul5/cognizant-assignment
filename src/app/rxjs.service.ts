08e2da6dd5e9a3af908eb69a74fc2f90a1073bfb
Shajidul Kibria <shajidul5@gmail.com>
2019-06-10 10:58:25 -0400

    Finished rxjs component

--- src/app/rxjs.service.ts
+++ src/app/rxjs.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RxjsService {
 
-  combineduser: {};
+  combinedUser = {};
 
   constructor(private http: HttpClient) { }
   
   paralell(){
-    return 
+    let user1 = this.http.get('https://jsonplaceholder.typicode.com/users/1');
+    let user2 = this.http.get('https://jsonplaceholder.typicode.com/users/2');
+
+    forkJoin([user1, user2]).subscribe(users => {
+      this.combinedUser["user1"] = users[0];
+      this.combinedUser["user2"] = user1[0];
+      console.log("this is the combined users after parallel http requests: ");
+      console.log(this.combinedUser);
+    })
+  }
+
+  sequential(){
+    this.http.get('https://jsonplaceholder.typicode.com/users/1').subscribe(user1 => {
+      this.combinedUser["user1"] = user1;
+      this.http.get('https://jsonplaceholder.typicode.com/users/2').subscribe(user2 => {
+        this.combinedUser["user2"] = user2;
+        console.log("This is the combined users after sequential http requests: ");
+        console.log(this.combinedUser);
+      })
+    })
   }
 }
--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -11,6 +11,15 @@ export class RxjsComponent implements OnInit {
   constructor(private rxjsService: RxjsService) { }
 
   ngOnInit() {
+    let t0 = performance.now();
+    this.rxjsService.sequential();
+    let t1 = performance.now();
+    console.log("The requests in sequential take " + (t1-t0) + " milliseconds!")
+
+    let t2 = performance.now();
+    this.rxjsService.paralell();
+    let t3 = performance.now();
+    console.log("The requests in parallel take " + (t3-t2) + " milliseconds!")
   }
 
 
